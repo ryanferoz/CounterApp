ba40ddd5806733202fea5c71f6a9f108e5641953
Ryan Feroz <ryanferoz@ryans-mbp-2.resource.campus.njit.edu>
2023-01-26 16:31:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tapcounterapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.tapcounterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var counter = 0
+        val upgradeButton = findViewById<Button>(R.id.upgradeBtn)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            // Handle user interaction here
+            //Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
+            val textView = findViewById<TextView>(R.id.textView)
+            button.setOnClickListener {
+                counter++
+                textView.text = counter.toString()
+                if (counter >= 100) {
+
+                    // Show upgrade button and set onClickListener
+                    upgradeButton.visibility = View.VISIBLE
+                    upgradeButton.setOnClickListener {
+                        button.text = "Add 2"
+
+                        // Update original button to add 2 instead of `
+                        button.setOnClickListener {
+                            counter += 2
+                            textView.text = counter.toString()
+                        }
+
+                        // Hide upgrade button again
+                        upgradeButton.visibility = View.INVISIBLE
+                    }
+                }
+            }
+        }
+    }}\
